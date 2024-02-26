11bc0b0126ceae264d64f25f576b19f9c9b98b82
Sandi Pradana <sandipradana.id@gmail.com>
2024-02-26 22:45:51 +0700

    factorial

--- main.go
+++ main.go
@@ -33,16 +33,29 @@ func Triangle(row int) {
 	}
 }
 
+func Factorial(n int) int {
+	total := n
+
+	for i := n - 1; i > 0; i-- {
+		total = total * i
+	}
+
+	return total
+}
+
 func main() {
 	fmt.Println("Test 1")
 	fmt.Printf("radar is palindrome? %t\n", Palindrome("radar"))
 	fmt.Printf("hello is palindrome? %t\n", Palindrome("hello"))
 
 	fmt.Println("Test 2")
-	fmt.Printf("max number from [3, 5, 1, 9, 2]  %d\n", Max([]int{3, 5, 1, 9, 2}))
-	fmt.Printf("max number from [1, 2, 3, 4, 5]  %d\n", Max([]int{1, 2, 3, 4, 5}))
-	fmt.Printf("max number from [5, 4, 3, 2, 1]  %d\n", Max([]int{5, 4, 3, 2, 1}))
+	fmt.Printf("max number from [3, 5, 1, 9, 2] %d\n", Max([]int{3, 5, 1, 9, 2}))
+	fmt.Printf("max number from [1, 2, 3, 4, 5] %d\n", Max([]int{1, 2, 3, 4, 5}))
+	fmt.Printf("max number from [5, 4, 3, 2, 1] %d\n", Max([]int{5, 4, 3, 2, 1}))
 
 	fmt.Println("Test 3")
 	Triangle(5)
+
+	fmt.Println("Test 4")
+	fmt.Printf("factorial dari 5 = %d\n", Factorial(5))
 }
