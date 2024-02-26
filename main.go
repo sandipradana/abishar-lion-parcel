cd166857a0d431941568d90f140d7cb1fb6428a0
Sandi Pradana <sandipradana.id@gmail.com>
2024-02-26 22:34:06 +0700

    triangle

--- main.go
+++ main.go
@@ -24,6 +24,15 @@ func Max(numbers []int) int {
 	return max
 }
 
+func Triangle(row int) {
+	for i := 0; i < row; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println("")
+	}
+}
+
 func main() {
 	fmt.Println("Test 1")
 	fmt.Printf("radar is palindrome? %t\n", Palindrome("radar"))
@@ -33,4 +42,7 @@ func main() {
 	fmt.Printf("max number from [3, 5, 1, 9, 2]  %d\n", Max([]int{3, 5, 1, 9, 2}))
 	fmt.Printf("max number from [1, 2, 3, 4, 5]  %d\n", Max([]int{1, 2, 3, 4, 5}))
 	fmt.Printf("max number from [5, 4, 3, 2, 1]  %d\n", Max([]int{5, 4, 3, 2, 1}))
+
+	fmt.Println("Test 3")
+	Triangle(5)
 }
