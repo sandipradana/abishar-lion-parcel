66162838fb21e67cba4f9214b39155195516999f
Sandi Pradana <sandipradana.id@gmail.com>
2024-02-26 22:29:53 +0700

    maximum number

--- main.go
+++ main.go
@@ -12,8 +12,25 @@ func Palindrome(word string) bool {
 	return word == reversed
 }
 
+func Max(numbers []int) int {
+	max := numbers[0]
+
+	for _, number := range numbers {
+		if number > max {
+			max = number
+		}
+	}
+
+	return max
+}
+
 func main() {
 	fmt.Println("Test 1")
 	fmt.Printf("radar is palindrome? %t\n", Palindrome("radar"))
 	fmt.Printf("hello is palindrome? %t\n", Palindrome("hello"))
+
+	fmt.Println("Test 2")
+	fmt.Printf("max number from [3, 5, 1, 9, 2]  %d\n", Max([]int{3, 5, 1, 9, 2}))
+	fmt.Printf("max number from [1, 2, 3, 4, 5]  %d\n", Max([]int{1, 2, 3, 4, 5}))
+	fmt.Printf("max number from [5, 4, 3, 2, 1]  %d\n", Max([]int{5, 4, 3, 2, 1}))
 }
